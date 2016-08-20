dd6ebe2f9cb93e2ae4ab905a488e044e6e75f248
Yamil Elías <yamileliassoto@gmail.com>
2016-08-20 12:43:47 -0600

    Ready compilation, but it doesn't show on page

---
+++ js/handlebars/data.js
@@ -0,0 +1,27 @@
+/* 
+ * This file contains the data for the portfolio.
+ * The main object, "portfolio" contains an array of projects
+ */
+
+var portfolio = {
+	item : [
+		{
+			img : "assets/img/portfolio/portfolio_09.png", 
+			type : "WIX WEB SITE",
+			zoom : "assets/img/portfolio/portfolio_09.png",
+			href : "http://igipmexico.wix.com/resourcecenter"
+		},
+		{
+			img : "assets/img/portfolio/portfolio_02.png", 
+			type : "WIX WEB SITE",
+			zoom : "assets/img/portfolio/portfolio_02.png",
+			href : "http://igipmexico.wix.com/wolfacademy"
+		},
+		{
+			img : "assets/img/portfolio/portfolio_03.png", 
+			type : "WORDPRESS SITE",
+			zoom : "assets/img/portfolio/portfolio_03.png",
+			href : "http://campuscity.com.mx/"
+		},
+	]
+};
